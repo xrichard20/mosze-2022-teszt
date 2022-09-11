a416038df3441c47c91fd9244d811b3eaa0fded4
Richard Marmarosi <xrichard20@gmail.com>
2022-09-11 18:41:11 +0200

    kommentek

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //helyesen N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //'' helyett "", nincs ;
+    for (int i = 0;) //paraméter hiány
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //így benne van a 0 is
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //2. paraméter hibás
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //meg kell adni melyik értéket jelenítse meg, hiányzik a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //pontatlan érték
+    for (int i = 0; i < N_ELEMENTS, i++) //',' helyett ";"
     {
-        atlag += b[i]
+        atlag += b[i] //nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
